96bf2b2da1f551f905fb515781b413c0a0edf0e3
BudakCS255 <148829262+BudakCS255@users.noreply.github.com>
2023-11-10 00:19:40 +0800

    Add files via upload

---
+++ test_db_connection.php
@@ -0,0 +1,18 @@
+<?php
+// Database configuration
+$dbHost = '188.166.220.38'; // Change to the actual database host if it's not on the same server
+$dbUser = 'root'; // Replace with your MySQL username
+$dbPass = ''; // Replace with your MySQL password
+$dbName = 'mywebsite_images'; // Replace with your MySQL database name
+
+// Attempt to connect to the database
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to the database!";
+$conn->close();
+?>
