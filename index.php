d814c49994151e8c10444e7aa26ac4931a7bcae5
BudakCS255 <148829262+BudakCS255@users.noreply.github.com>
2023-11-10 01:50:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+// Database configuration
+$dbHost = '188.166.220.38'; // Change to the actual database host if it's not on the same server
+$dbUser = 'root'; // Replace with your MySQL username
+$dbPass = ' '; // Replace with your MySQL password
+$dbName = 'mywebsite_images'; // Replace with your MySQL database name
+
 <?php
-echo gethostname();
+// Database configuration
+$dbHost = 'localhost'; // Change to the actual database host if it's not on the same server
+$dbUser = 'root'; // Replace with your MySQL username
+$dbPass = ''; // Replace with your MySQL password
+$dbName = 'mywebsite_images'; // Replace with your MySQL database name
+
+// Attempt to connect to the database
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Mission Failed: Connection failed: " . $conn->connect_error);
+} else {
+    echo "Mission Accomplished: Connected successfully to the database!";
+}
+
+$conn->close();
 ?>
